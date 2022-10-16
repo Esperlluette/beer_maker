c5ec1b64c79f7fbce88867ef1ada4d7c9eb0b1cc
esperlluette <janl.mannessier@gmail.com>
2022-10-16 11:45:05 +0200

    à fix ERROR : type 'Future<dynamic>' is not a subtype of type 'Map<String, dynamic>'

--- lib/pages/beerlisting.dart
+++ lib/pages/beerlisting.dart
@@ -6,6 +6,7 @@ import 'package:beer_maker/resources/object/appcolor.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
+
 class Beerlist extends StatefulWidget {
   const Beerlist({super.key});
 
@@ -14,27 +15,68 @@ class Beerlist extends StatefulWidget {
 }
 
 class BeerlistState extends State<Beerlist> {
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
+      GlobalKey<RefreshIndicatorState>();
 
-  Future<Map<String, dynamic>> fetchBeersList() async{
+  Future<Map<String, dynamic>> fetchBeersList() async {
     final response = await http.get(Uri.parse(
         'https://s3-4013.nuage-peda.fr/beer_maker/public/api/beers?page=1'));
 
-    if (response.statusCode == 200){
-      log(response.body.toString());
-     return convert.jsonDecode(response.body);
-    }else {
+    if (response.statusCode == 200) {
+      log(response.body[0].toString());
+      return convert.jsonDecode(response.body);
+    } else {
       throw Exception(StringsFR.fetchException);
     }
   }
 
+  init() async{
+    Map<String, dynamic> map = await fetchBeersList();
+    return map;
+  }
+
   @override
   Widget build(BuildContext context) {
+    Map<String, dynamic> map = init();
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(StringsFR.beerlist),
-        backgroundColor: const Color(AppColor.appcolor),
-      ),
-      body: BeerCardFactrory.factory(fetchBeersList().then((value) => Iterable<dynamic>)),
-    );
+        appBar: AppBar(
+          title: const Text(StringsFR.beerlist),
+          backgroundColor: const Color(AppColor.appcolor),
+        ),
+        body:
+            // Container(
+            // height: 200,
+            // width: 100,
+            // padding: const EdgeInsets.all(10),
+            // margin: const EdgeInsets.all(10),
+            // child:
+            //   Column(
+            // children: [
+            RefreshIndicator(
+                key: _refreshIndicatorKey,
+                color: Colors.white,
+                backgroundColor: Colors.blue,
+                strokeWidth: 4.0,
+                onRefresh: _refresh,
+                child: ListView.builder(
+                  itemCount: 25,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      onTap: () {
+                        log(fetchBeersList().toString());
+                      },
+                      title: Text('Item $index'),
+                    );
+                  },
+                ))
+        //   ],
+        // ),
+        );
+    // )
+    // );
+  }
+
+  Future<void> _refresh() async {
+    return Future<void>.delayed(const Duration(seconds: 3));
   }
 }
--- lib/resources/factory/beercardfactory.dart
+++ lib/resources/factory/beercardfactory.dart
@@ -1,9 +1,12 @@
+import 'dart:developer';
+
 import 'package:beer_maker/resources/stringsfr.dart';
 import 'package:flutter/material.dart';
 
 class BeerCardFactrory {
   static Column factory(map) {
     if (map != null) {
+      log(map.toString());
       Column col = Column(
         children: [
           for (var beer in map) ...{
